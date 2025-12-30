ffd2e27e5fd215230e19a4cc7311c395a3164216
Dmitriy Folomkin <dmitriy.folomkin@yandex.ru>
2025-12-30 15:03:31 +0300

    исправлены зависимости для тестов

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -3,6 +3,7 @@ package dev.folomkin
 import io.ktor.http.ContentType
 import io.ktor.server.application.*
 import io.ktor.server.http.content.staticResources
+import io.ktor.server.plugins.NotFoundException
 import io.ktor.server.plugins.statuspages.StatusPages
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -10,6 +11,9 @@ import io.ktor.server.routing.*
 fun Application.configureRouting() {
     install(StatusPages) {
         exception<IllegalStateException> { call, cause ->
+            call.respondText("App in illegal state as ${cause.message}")
+        }
+        exception<NotFoundException> { call, cause ->
             call.respondText("App in illegal state as ${cause.message}")
         }
     }
@@ -25,6 +29,9 @@ fun Application.configureRouting() {
         staticResources("/content", "mycontent")
         get("/error-test") {
             throw IllegalStateException("Too Busy")
+        }
+        get("/error-test-404") {
+            throw NotFoundException("Resource not found")
         }
     }
 }
